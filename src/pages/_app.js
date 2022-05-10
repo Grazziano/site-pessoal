3001cdd3cf939fec19cf324b9f2de585c2bf6d8a
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-05-10 20:50:15 -0300

    Component Header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '../../styles/globals.css';
 import Head from 'next/head';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Grazziano | Dev</title>
       </Head>
+      <Header />
       <Component {...pageProps} />
     </>
   );
