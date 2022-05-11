54ff8f0423158a63f58bc962bb08e375fd1ae039
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-05-10 21:16:25 -0300

    Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { FaArrowRight } from 'react-icons/fa';
+import {
+  Container,
+  Content,
+  Infos,
+  Name,
+  Function,
+  Intro,
+  LinkProjects,
+  Logo,
+  Img,
+} from '../../styles/indexStyle';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <h1>teste</h1>
-  )
+    <Container>
+      <Content>
+        <Infos>
+          <Name>Olá, sou Grazziano</Name>
+          <Function>FullStack Developer</Function>
+          <Intro>
+            Graduado em Análise e Desenvolvimento de sistemas pela Faculdade de
+            Tecnologia SENAC RS, e em desenvolvimento web pela Trybe, tenho
+            experiência em desenvolvimento Web (React, Node, JS, MySQL e
+            outros).
+          </Intro>
+          <Link href="projects">
+            <LinkProjects> PROJETOS <FaArrowRight /></LinkProjects>
+          </Link>
+        </Infos>
+        <Logo>
+          <Img src="/images/foto-perfil.jpg" alt="logo" />
+        </Logo>
+      </Content>
+    </Container>
+  );
 }
